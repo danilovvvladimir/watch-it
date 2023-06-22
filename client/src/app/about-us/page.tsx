71e096558f26d4776dfc6ff7d4162ab9271f31a9
Vladimir <fruityflunt@gmail.com>
2023-06-22 06:40:15 +0300

    About Page

--- client/src/app/about-us/page.tsx
+++ client/src/app/about-us/page.tsx
@@ -4,6 +4,8 @@ import { FC } from "react";
 
 // ==> Other imports <===
 import "./AboutUsPage.scss";
+import Image from "next/image";
+import decorations from "@/assets/images/decorations/about-us-decorations.svg";
 
 export const metadata = {
   title: "Watch IT | About Us",
@@ -16,24 +18,25 @@ const AboutUsPage: FC = () => {
     <section className="about-us-page">
       <div className="container">
         <h1 className="title about-us-page__title">
-          Stories move us. <br />
+          Stories move <span>us.</span> <br />
           They make us feel more emotion, see new perspectives, and bring us
+          <br />
           closer to each other.
         </h1>
 
         <p className="about-us-page__description">
-          At Netflix, we want to entertain the world. Whatever your taste, and
-          no matter where you live, we give you access to best-in-class TV
-          series, documentaries, feature films and mobile games. Our members
-          control what they want to watch, when they want it, in one simple
-          subscription.
+          We&apos;re streaming in more than 30 languages and 190 countries,
+          because great stories can come from anywhere and be loved everywhere.
         </p>
         <p className="about-us-page__description">
-          We're streaming in more than 30 languages and 190 countries, because
-          great stories can come from anywhere and be loved everywhere. We are
-          the world's biggest fans of entertainment, and we're always looking to
-          help you find your next favorite story.
+          We are the world&apos;s biggest fans of entertainment, and we&apos;re
+          always looking to help you find your next favorite story.
         </p>
+        <Image
+          src={decorations}
+          alt="decoration"
+          className="about-us-page__decorations"
+        />
       </div>
     </section>
   );
